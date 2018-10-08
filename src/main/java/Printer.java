eccaede1fbf6258b2cf182a543f3cc249e67ddb4
GerCassa <gercassa@gmail.com>
2018-10-08 17:29:27 +0100

    added toner, and passed all test. Extension completed

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,33 @@
 public class Printer {
+    private int paper;
+    private int toner;
+
+    public Printer(int paper, int toner){
+        this.paper = paper;
+        this.toner = toner;
+    }
+
+
+    public int paperLoad() {
+        return this.paper;
+    }
+
+    public int print(int a, int b) {
+        int pages = a * b;
+        if (this.paper >= pages) {
+            this.paper = this.paper - pages;
+            this.toner = this.toner - pages;
+            return this.paper;
+        }
+        return pages;
+    }
+
+    public int refill() {
+        while (this.paper < 200) this.paper++;
+        return this.paper;
+    }
+
+    public int toner() {
+        return this.toner;
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class PrinterTest {
+
+    Printer printer;
+
+    @Before
+    public void before() { printer = new Printer(200, 100);}
+
+    @Test
+    public void hasPaper() {
+        assertEquals(200, printer.paperLoad());
+    }
+
+    @Test
+    public void canPrint() {
+        assertEquals(190, printer.print(5,2));
+        assertEquals(90, printer.toner());
+
+    }
+
+    //    @Test
+//    public void cannotPrint() {
+//        assertEquals("Not enough paper/toner to perform the action", printer.print(60, 2));
+//    }
+
+    @Test
+    public void canRefill() {
+        assertEquals(200, printer.refill());
+    }
+
+    @Test
+    public void hasToner() {
+        assertEquals(100, printer.toner());
+    }
+
 }
