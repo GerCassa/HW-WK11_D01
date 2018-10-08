952bdaa8c2463c55acab2d02113401ddf41c3663
GerCassa <gercassa@gmail.com>
2018-10-08 16:29:08 +0100

    added waterbottle and printer classes

---
+++ src/main/java/WaterBottle.java
@@ -0,0 +1,11 @@
+public class WaterBottle {
+    private int vol;
+
+    public WaterBottle(int vol) {
+        this.vol = vol;
+    }
+
+    public int getVolume() {
+        return this.vol;
+    }
+}
